41353ca4c3b771ef0a187d6ac574074a922f6bd2
That Girl <codegrl@gmail.com>
2011-07-03 09:37:25 -0400

    Changed directory sturcture and cleaned up files to add a category to the main django structure
    C
    
    C

---
+++ main/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    Name = models.CharField(max_length=255)
+    def __unicode__(self):
+        return self.Name
+    class Admin:
+        pass
+    class Meta:
+        pass
